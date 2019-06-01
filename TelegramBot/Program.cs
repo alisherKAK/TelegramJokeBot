ed383589eedaf2d9e27e19e5334742f5768e38dd
AlisherKaal <alisherkala007@gmail.com>
2019-06-02 00:54:08 +0600

    Telegram Joke Bot

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -10,6 +10,11 @@ namespace TelegramBot
     {
         static void Main(string[] args)
         {
+            TelegramBot bot = new TelegramBot();
+
+            bot.Open();
+            Console.ReadLine();
+            bot.Close();
         }
     }
 }
---
+++ TelegramBot/TelegramBot.cs
@@ -0,0 +1,43 @@
+﻿using AIMLbot;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    public class TelegramBot
+    {
+        private TelegramBotClient bot = new TelegramBotClient("899627781:AAHP8Wl8eEP_rZ_rLiz5RB3JQv-zyIWE7yY");
+
+        public TelegramBot()
+        {
+            bot.OnMessage += BotOnMessage;
+            bot.OnMessageEdited += BotOnMessage;
+        }
+
+        public void Open()
+        {
+            bot.StartReceiving();
+        }
+
+        public void Close()
+        {
+            bot.StopReceiving();
+        }
+
+        private void BotOnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                Bot AI = new Bot();
+                AI.loadSettings(); //It will Load Settings from its Config Folder with this code
+                AI.loadAIMLFromFiles(); //With this Code It Will Load AIML Files from its AIML Folder
+                AI.isAcceptingUserInput = false; //With this Code it will Disable UserInput For Now
+                User myuser = new User("Username Here", AI); //With This Code We Will Add The User Through AI/Bot
+                AI.isAcceptingUserInput = true; //Now The User Input is Enabled Again with this Code
+                Request r = new Request(e.Message.Text, myuser, AI); //With This Code it will Request The Response From AIML Folders
+                Result res = AI.Chat(r); //With This Code It Will Get Result
+
+                bot.SendTextMessageAsync(e.Message.Chat.Id, res.Output);
+            }
+        }
+    }
+}
